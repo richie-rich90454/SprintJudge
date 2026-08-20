4af0fa0133efcf1049f50a08409b06dc6c3f0cda
richie-rich90454 <rjiang880@gmail.com>
2026-08-20 19:55:34 +0800

    feat(backend): add Ids (src/main/java/com/openquiz/util/Ids.java)

---
+++ src/main/java/com/openquiz/util/Ids.java
@@ -0,0 +1,17 @@
+package com.openquiz.util;
+
+import java.util.UUID;
+
+public final class Ids {
+
+    private Ids() {}
+
+    public static String uuid() {
+        return UUID.randomUUID().toString();
+    }
+
+    public static String pin() {
+        int n = (int) (Math.random() * 900000) + 100000;
+        return String.valueOf(n);
+    }
+}
