2559c4519526fcea394031173a4ddf623bbbbe1d
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 17:00:52 +0800

    test(backend-test): update Game Room Manager Test (src/test/java/com/sprintjudge/service/GameRoomManagerTest.java)

--- src/test/java/com/sprintjudge/service/GameRoomManagerTest.java
+++ src/test/java/com/sprintjudge/service/GameRoomManagerTest.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
