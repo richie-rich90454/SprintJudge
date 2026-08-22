bb0393509884a755cf3f17d54d9d102389a783f2
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 12:17:32 +0800

    refactor(backend): rebrand Import Export Service to SprintJudge namespace (src/main/java/com/sprintjudge/service/ImportExportService.java)

--- src/main/java/com/sprintjudge/service/ImportExportService.java
+++ src/main/java/com/sprintjudge/service/ImportExportService.java
@@ -1,13 +1,13 @@
-package com.openquiz.service;
+package com.sprintjudge.service;
 
-import com.openquiz.domain.dto.export.ExportBundle;
-import com.openquiz.domain.models.Question;
-import com.openquiz.domain.models.Quiz;
-import com.openquiz.repository.AdminSettingsRepository;
-import com.openquiz.repository.QuestionRepository;
-import com.openquiz.repository.QuizRepository;
-import com.openquiz.util.Ids;
-import com.openquiz.util.Json;
+import com.sprintjudge.domain.dto.export.ExportBundle;
+import com.sprintjudge.domain.models.Question;
+import com.sprintjudge.domain.models.Quiz;
+import com.sprintjudge.repository.AdminSettingsRepository;
+import com.sprintjudge.repository.QuestionRepository;
+import com.sprintjudge.repository.QuizRepository;
+import com.sprintjudge.util.Ids;
+import com.sprintjudge.util.Json;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
