1662cd0cc2d22f08f739f40c75706d17d6f3e544
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 17:00:52 +0800

    test(backend-test): update Admin Settings Service Test (src/test/java/com/sprintjudge/service/AdminSettingsServiceTest.java)

--- src/test/java/com/sprintjudge/service/AdminSettingsServiceTest.java
+++ src/test/java/com/sprintjudge/service/AdminSettingsServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
